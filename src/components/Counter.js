d0f126889711f7414b304d0bba9311e433ffa3fb
Eric <egant43123@gmail.com>
2021-07-12 12:05:24 -0400

    e

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,26 +46,37 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
+import ReactDOM from "react-dom"
+
 
 export default function Counter() {
   /* STEP 1 */
-
+const [count, setCount] = useState(0) 
   const increment = () => {
-    /* STEP 4 */
+    setCount(count + 1)
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1)
   };
   const reset = () => {
     /* STEP 6 */
   };
 
-  const style = {
+  let style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
     color: 'royalblue', /* STEP 2 */
   };
+  if(count % 2 != 0){
+    style = {
+      fontSize:'1.5em',
+      marginBottom: '0.3em',
+      color: 'crimson'
+    }
+  }
+  
 
   return (
     <div className='widget-counter container'>
